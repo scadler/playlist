6ed4b401223ae11cda9ca6aaa12ec43dcb4ef8b3
Scott Adler <anorangepi@gmail.com>
2018-12-12 01:56:31 +0000

    Arrays

--- js/script.js
+++ js/script.js
@@ -2,13 +2,47 @@
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
 var songs = [
-    "24 Karat Magic",
-    "Nice For What",
-    "Universe"
+    "Friday",
+    "Despacito",
+    "Gangnam Style",
+    "Dynamite",
+    "Sandstorm",
+    "Ultimate Showdown",
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
+var image_links = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    ];
+var artists = [
+    "Rebecca Black",
+    "Luis Fonsi",
+    "Psy",
+    "Taio Cruz",
+    "Darude",
+    "f",
+    ];
+var songLengths = [
+    "3:30",
+    "3:47",
+    "4:33",
+    "3:22",
+    "7:26",
+    "f",
+    ];
+var song_links = [
+    "https://www.youtube.com/watch?v=kfVsfOSbJY0",
+    "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "https://www.youtube.com/watch?v=9bZkp7q19f0",
+    "https://www.youtube.com/watch?v=Vysgv7qVYTo",
+    "https://www.youtube.com/watch?v=y6120QOlsfU",
+    "f",
+    ];
 
 
 
